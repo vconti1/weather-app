f2c32eb0324f52474a690ca2c7c5a3787d3601cb
vconti1 <vincentconti393@gmail.com>
2025-07-31 13:14:56 -0400

    continue work on fetchWeather

--- services/fetchWeather.ts
+++ services/fetchWeather.ts
@@ -1,14 +1,17 @@
 
-export const fetchWeatherData = async(location : String)=>{
+export const fetchWeather = async(location : String)=>{
     // location may be city name or zip code
+    let res;
     const key = process.env.API_KEY;
-    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`; 
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&lang=English`; 
 
     try{
-        const res = await fetch(url);
+         res = await fetch(url);
+         const jsonData = await res.json();
+         return jsonData;
     }catch(err){
         console.error("Error fetching data.", err);
     }
-
     return;
 }
+

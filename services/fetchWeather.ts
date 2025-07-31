a4d25fbd7f864083cdc3994cdbaa64bd24ed68c8
vconti1 <vincentconti393@gmail.com>
2025-07-30 20:09:17 -0400

    add fetchWeather function

--- services/fetchWeather.ts
+++ services/fetchWeather.ts
@@ -1,6 +1,14 @@
 
-export const fetchWeatherData = async()=>{
+export const fetchWeatherData = async(location : String)=>{
+    // location may be city name or zip code
+    const key = process.env.API_KEY;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}`; 
 
+    try{
+        const res = await fetch(url);
+    }catch(err){
+        console.error("Error fetching data.", err);
+    }
 
-    
+    return;
 }

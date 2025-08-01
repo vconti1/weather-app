cd47a6a781f6b532874bed1e558543cf865c5c47
vconti1 <vincentconti393@gmail.com>
2025-08-01 18:58:13 -0400

    rate limit by ip

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,23 +1,14 @@
 import { redis } from "@/lib/db";
-import { ratelimit } from "@/middleware/middleware"
 import { NextResponse } from "next/server";
+import { ratelimit } from "@/middleware"
 
 
 
 export const GET = async () => {
     const result = await redis.get('foo');
-    //const ip = getClientIp(req: Request) || 'unknown';
 
-    const ratelimitResult = await ratelimit.limit('test');
-
-    if(!ratelimitResult.success){
-        return NextResponse.json({ error: "Too many requests" }, { status: 429 });
-    }
-
-    return NextResponse.json({
-        result,
-        ratelimitResult,
-    });
+    return NextResponse.json({ data: result });
+    
 }
 
 
---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis'
+import { NextRequest, NextResponse } from 'next/server';
+
+console.log("working");
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+ export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "30 s"), // allow p1 requests per p2 seconds
+});
+
+export async function middleware(req: NextRequest){
+    const ipHeader = req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip');
+    const ip = ipHeader ? ipHeader.split(',')[0].trim() : 'unknown';
+
+    const { success } = await ratelimit.limit(ip);
+
+    if(!success){
+        return NextResponse.json({ error: "Too many requests. Try again later. " }, { status: 429 })
+    }
+    
+    return NextResponse.next();
+};
+
+
+export const config = {
+  matcher: ['/api/:path*'],
+};
+

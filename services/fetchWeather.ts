7bc220c2fbccc4775df9f6bcbd9a2c6376630bcb
vconti1 <vincentconti393@gmail.com>
2025-08-01 20:19:17 -0400

    push straggler

--- services/fetchWeather.ts
+++ services/fetchWeather.ts
@@ -1,18 +1,10 @@
 
-export const fetchWeather = async(location : String)=>{
-    // location may be city name or zip code
-    let res;
-    const key = process.env.NEXT_PUBLIC_API_KEY;
-    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&lang=English`; 
+export const fetchWeather = async (location: string) => {
+  const res = await fetch(`/api/weather?query=${encodeURIComponent(location)}`);
+  if (!res.ok){
+     throw new Error("Failed to fetch weather data");
+  }
+  return res.json();
+};
 
-    try{
-         res = await fetch(url);
-         const jsonData = await res.json();
-        // console.log("this function is working");
-         return jsonData;
-    }catch(err){
-        console.error("Error fetching data.", err);
-    }
-    return;
-}
-
+  //const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&lang=English`; 

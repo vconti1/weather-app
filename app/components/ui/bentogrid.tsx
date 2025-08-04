b0ea0291c2d44ab9a8204cd1efc134e057b7c624
vconti1 <vincentconti393@gmail.com>
2025-08-04 14:39:41 -0400

    finish layout for primary bentogird

--- app/components/ui/bentogrid.tsx
+++ app/components/ui/bentogrid.tsx
@@ -22,43 +22,49 @@ export const BentoGrid = ({
 export const BentoGridItem = ({
   className,
   title,
-  description,
+  data,
   condition,
-  header,
   img,
+  time,
   spareImg,
   icon,
   imgClassName,
   titleClassName,
   temp,
+  forecast,
   tempRange,
 }: {
   className?: string;
   title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
+  data?: any;
   condition?: string | React.ReactNode;
   header?: React.ReactNode;
   img? : string;
+  time?: string;
   imgClassName?: string;
   titleClassName?: string;
   spareImg? : string;
-  icon?: React.ReactNode;
+  icon?: string;
   temp?: string
+  forecast?: string;
   tempRange?: string;
 
 }) => {
+   //console.log("BentoGrid received data:", data);
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-row justify-between space-y-4 rounded-xl border border-neutral-200 p-10 transition duration-200 hover:shadow-xl border-white/[1.0] shadow-none",
+        "group/bento row-span-1 w-full flex flex-row justify-between rounded-xl border border-neutral-200 p-10 transition duration-200 border-white/[1.0] shadow-none overflow-hidden",
         className,
       )}
     >
-      {/* START LEFT COLUMN */}
-      <div className = "flex flex-row">
-        <div className="transition duration-200">
-          {icon}
-          <div className="mt-2 mb-2 font-sans font-bold text-5xl">
+      {/* START TOP ROW */}
+
+      <div className = "flex flex-col w-full">
+      <div className = "flex flex-row pb-10">
+
+      <div className = "flex flex-1 min-w-0 flex-col w-217 ">
+          <div className="relative min-w-0 mt-2 mb-2 font-sans font-bold text-5xl overflow-hidden whitespace-nowrap truncate">
             {title}
           </div>
           <div className = "font-sans text-5xl">
@@ -72,14 +78,26 @@ export const BentoGridItem = ({
           </div>
           
         </div>
-      </div>
-      {/* END LEFT COLUMN */}
-
-      {/* START RIGHT COLUMN */}
-      <div className = "">
+      <div className = "flex-shrink-0">
       <img src = {img}/>
       </div>
+      </div>
 
+      <div className="flex justify-center pt-5">
+        <div className="items-center h-32 w-full max-w-4xl overflow-x-auto overflow-y-hidden bg-transparent rounded-lg scroll-smooth whitespace-nowrap thin-scrollbar ">
+              {/* This is where scroll content goes */}
+              {data.map((hour:any, index:number) => (
+                 <div key={index} className="inline-block text-center w-24 mx-2 text-white">
+                  <p className = "text-sm">{hour.time}</p>
+                  <img src={hour.icon} alt="weather icon" className="mx-auto" />
+                  <p>{hour.temp_f}°</p>
+                </div>
+              ))}
+
+        </div> 
+      </div>
+
+      </div>
     </div>
   );
 };
--- app/weather-details/page.tsx
+++ app/weather-details/page.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from 'react';
 import { BentoGridItem } from "../components/ui/BentoGrid";
+import { BentoGridDaysItem } from "../components/ui/BentoGridDays";
 
 
 export default function WeatherDetailsPage() {
@@ -34,7 +35,59 @@ export default function WeatherDetailsPage() {
     fetchWeather();
   }, [location]);
 
-  
+
+  function nextTwentyFourHours(){
+
+    if (!weather || !weather.location || !weather.location.localtime) {
+      return []; 
+    }
+    let forecastOfTheDay: any[] = [];
+    const nextHours = 24;
+    const localTimeString = weather.location.localtime.split(' ')[1]; // looks like "15:00", "0:00", "7:00" etc. (goes up to "23:00")
+    let hour = parseInt(localTimeString?.split(':')[0], 10);
+    let hourInfo;
+
+   for(let i = 0, day = 0; i < nextHours; i++, hour++){
+
+    if(forecastOfTheDay.length === 0){
+      hourInfo = weather.forecast.forecastday[day].hour[hour];
+      const customHour = {
+        time: `NOW`, 
+        temp_f: hourInfo.temp_f,
+        icon: `https:${hourInfo.condition.icon}`,
+      };
+      forecastOfTheDay.push(customHour);
+    }else if(hour===24){
+      day = 1; // index of following day
+      hour = 0; //reset clock
+      hourInfo = weather.forecast.forecastday[day].hour[hour];
+      const customHour = {
+        time: `12AM`, //can only ever be 12AM due to conditional
+        temp_f: hourInfo.temp_f,
+        icon: `https:${hourInfo.condition.icon}`,
+      };
+       forecastOfTheDay.push(customHour);
+    }else if(hour===12){
+      const customHour = {
+        time: `12PM`, //can only ever be 12PM due to conditional
+        temp_f: hourInfo.temp_f,
+        icon: `https:${hourInfo.condition.icon}`,
+      };
+       forecastOfTheDay.push(customHour);
+    }else{
+      hourInfo = weather.forecast.forecastday[day].hour[hour];
+      const customHour = {
+        time: hour > 12 ? `${(hour-12).toString()}PM` : `${hour.toString()}AM`,
+        temp_f: hourInfo.temp_f,
+        icon: `https:${hourInfo.condition.icon}`,
+      };
+       forecastOfTheDay.push(customHour);
+      }
+    }
+    return forecastOfTheDay;
+  }
+  const result = nextTwentyFourHours();
+  console.log(result);
 
   return (
     <div className="p-8 items-center">
@@ -53,14 +106,17 @@ export default function WeatherDetailsPage() {
             //header = {weather.location.country}
             title = {weather.location.name}
             temp = {`${weather.current.temp_f}°`}
-            tempRange = {`H:${weather.forecast.forecastday[0].day.maxtemp_f}° L:${weather.forecast.forecastday[0].day.mintemp_f}°`}
+            tempRange = {`H: ${weather.forecast.forecastday[0].day.maxtemp_f}° L: ${weather.forecast.forecastday[0].day.mintemp_f}°`}
             condition = {`${weather.current.condition.text}`}
             img = {`https:${weather.current.condition.icon.replace("64x64", "128x128")}`}
+            data = {result}
             />
+            
             </div>
 
+
             <div className = "pt-5">
-              <BentoGridItem className = "h-60 w-150"
+              <BentoGridDaysItem className = "h-60 w-150"
               
               />
             </div>

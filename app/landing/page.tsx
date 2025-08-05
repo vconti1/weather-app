a9b92d52bebe364b81c838da578a45c1a4015a62
vconti1 <vincentconti393@gmail.com>
2025-08-04 20:15:33 -0400

    handle errors on input

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -9,13 +9,19 @@ export default function LandingPage() {
 
 const [data, setData] = useState<any>(null);
 const [input, setInput] = useState('');
+const [error, setError] = useState('');
 const router = useRouter();
 
 const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const res = await fetchWeather(input);
     setData(res);
+    if(!res.error){
     router.push(`/weather-details?location=${encodeURIComponent(input)}`);
+    }
+    else{
+      setError('Could not fetch weather data. Make sure the city or zip code you are entering is valid.');
+    }
 }
 
   return (
@@ -31,6 +37,9 @@ const handleSubmit = async (e: React.FormEvent) => {
             className="border p-2 rounded w-full"
             />
            </form>
+           {error && (
+            <p className="text-red-500 mt-10 text-sm">{error}</p>
+            )}
     </div>
   );
 }
